9d5645cadc303abb20c655c91252ec72e127a590
Karol <karolcwiklik@o2.pl>
2023-05-14 20:54:02 +0200

    nav-desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,23 @@
-const test = () => {
-    console.log('czesc');
-    // komentarz
+const offerBtnDesktop = document.querySelector('.offer-desktop-item')
+const offerBtnDesktopChangeColor = document.querySelector('.offer-color')
+const dropdownDesktopItem = document.querySelector('.desktop-offer-menu')
+const arrowIconDesktopMenu = document.querySelector('.nav__arrow-icon')
+
+const showDekstopOfferMenu = () => {
+    dropdownDesktopItem.classList.add('active')
+    arrowIconDesktopMenu.classList.add('rotate')
+    offerBtnDesktopChangeColor.style.color = '#d768b2'
 }
+
+const hideDekstopOfferMenu = () => {
+    dropdownDesktopItem.classList.remove('active')
+    arrowIconDesktopMenu.classList.remove('rotate')
+    offerBtnDesktopChangeColor.style.color = '#333333'
+
+}
+
+
+offerBtnDesktop.addEventListener('mouseover', showDekstopOfferMenu)
+dropdownDesktopItem.addEventListener('mouseover', showDekstopOfferMenu)
+offerBtnDesktop.addEventListener('mouseleave', hideDekstopOfferMenu)
+dropdownDesktopItem.addEventListener('mouseleave', hideDekstopOfferMenu)
